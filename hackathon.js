94e9e961ad0820581e3ccb6bbdfa1bc859c09975
LeonardoBarc <35239708+LeonardoBarc@users.noreply.github.com>
2024-02-17 19:39:21 +0000

    Add files via upload

--- hackathon.js
+++ hackathon.js
@@ -1,112 +1,185 @@
-// Card images
-const images = ['🍎', '🍌', '🍒', '🍓', '🥝', '🍇', '🍊', '🍍'];
+// Memory Game
+// © 2014 Nate Wiley
+// License -- MIT
+// best in full screen, works on phones/tablets (min height for game is 500px..) enjoy ;)
+// Follow me on Codepen
 
-// Duplicate images to create pairs
-const cards = [...images, ...images];
+(function(){
+	
+	var Memory = {
 
-// Shuffle cards
-cards.sort(() => Math.random() - 0.5);
+		init: function(cards){
+			this.$game = $(".game");
+			this.$modal = $(".modal");
+			this.$overlay = $(".modal-overlay");
+			this.$restartButton = $("button.restart");
+			this.cardsArray = $.merge(cards, cards);
+			this.shuffleCards(this.cardsArray);
+			this.setup();
+		},
 
-// Initialize variables
-let flippedCards = [];
-let matchedCards = [];
-let flips = 0; // Track the total number of flips
+		shuffleCards: function(cardsArray){
+			this.$cards = $(this.shuffle(this.cardsArray));
+		},
 
-// Define custom rating levels and descriptions
-const ratingLevels = [
-    { flips: 10, description: '5 stars' },
-    { flips: 15, description: '4 stars' },
-    { flips: 20, description: '3 stars' },
-    { flips: 25, description: '2 stars' },
-    { flips: Infinity, description: '1 star' }
-];
+		setup: function(){
+			this.html = this.buildHTML();
+			this.$game.html(this.html);
+			this.$memoryCards = $(".card");
+			this.paused = false;
+     	this.guess = null;
+			this.binding();
+		},
 
-// Function to update the star rating based on the number of flips
-function updateStarRating() {
-    const starRating = document.getElementById('star-rating');
-    let description = '';
+		binding: function(){
+			this.$memoryCards.on("click", this.cardClicked);
+			this.$restartButton.on("click", $.proxy(this.reset, this));
+		},
+		// kinda messy but hey
+		cardClicked: function(){
+			var _ = Memory;
+			var $card = $(this);
+			if(!_.paused && !$card.find(".inside").hasClass("matched") && !$card.find(".inside").hasClass("picked")){
+				$card.find(".inside").addClass("picked");
+				if(!_.guess){
+					_.guess = $(this).attr("data-id");
+				} else if(_.guess == $(this).attr("data-id") && !$(this).hasClass("picked")){
+					$(".picked").addClass("matched");
+					_.guess = null;
+				} else {
+					_.guess = null;
+					_.paused = true;
+					setTimeout(function(){
+						$(".picked").removeClass("picked");
+						Memory.paused = false;
+					}, 600);
+				}
+				if($(".matched").length == $(".card").length){
+					_.win();
+				}
+			}
+		},
 
-    // Find the appropriate rating level based on the number of flips
-    for (const level of ratingLevels) {
-        if (flips <= level.flips) {
-            description = level.description;
-            break;
-        }
-    }
+		win: function(){
+			this.paused = true;
+			setTimeout(function(){
+				Memory.showModal();
+				Memory.$game.fadeOut();
+			}, 1000);
+		},
 
-    // Update the star rating display
-    starRating.textContent = description;
-}
+		showModal: function(){
+			this.$overlay.show();
+			this.$modal.fadeIn("slow");
+		},
 
-// Function to flip a card
-function flipCard() {
-    // Increment the total number of flips
-    flips++;
+		hideModal: function(){
+			this.$overlay.hide();
+			this.$modal.hide();
+		},
 
-    if (flippedCards.length < 2 && !flippedCards.includes(this) && !matchedCards.includes(this)) {
-        this.classList.add('flipped');
-        flippedCards.push(this);
+		reset: function(){
+			this.hideModal();
+			this.shuffleCards(this.cardsArray);
+			this.setup();
+			this.$game.show("slow");
+		},
 
-        // Toggle visibility of the card content
-        const content = this.querySelector('span');
-        content.classList.remove('hidden'); // Show the content
+		// Fisher--Yates Algorithm -- https://bost.ocks.org/mike/shuffle/
+		shuffle: function(array){
+			var counter = array.length, temp, index;
+	   	// While there are elements in the array
+	   	while (counter > 0) {
+        	// Pick a random index
+        	index = Math.floor(Math.random() * counter);
+        	// Decrease counter by 1
+        	counter--;
+        	// And swap the last element with it
+        	temp = array[counter];
+        	array[counter] = array[index];
+        	array[index] = temp;
+	    	}
+	    	return array;
+		},
 
-        if (flippedCards.length === 2) {
-            setTimeout(checkMatch, 1000);
-        }
-    }
+		buildHTML: function(){
+			var frag = '';
+			this.$cards.each(function(k, v){
+				frag += '<div class="card" data-id="'+ v.id +'"><div class="inside">\
+				<div class="front"><img src="'+ v.img +'"\
+				alt="'+ v.name +'" /></div>\
+				<div class="back"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/codepen-logo.png"\
+				alt="Codepen" /></div></div>\
+				</div>';
+			});
+			return frag;
+		}
+	};
 
-    // Update the star rating after every pair of cards flipped
-    updateStarRating();
-}
+	var cards = [
+		{
+			name: "php",
+			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/php-logo_1.png",
+			id: 1,
+		},
+		{
+			name: "css3",
+			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/css3-logo.png",
+			id: 2
+		},
+		{
+			name: "html5",
+			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/html5-logo.png",
+			id: 3
+		},
+		{
+			name: "jquery",
+			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/jquery-logo.png",
+			id: 4
+		}, 
+		{
+			name: "javascript",
+			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/js-logo.png",
+			id: 5
+		},
+		{
+			name: "node",
+			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/nodejs-logo.png",
+			id: 6
+		},
+		{
+			name: "photoshop",
+			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/photoshop-logo.png",
+			id: 7
+		},
+		{
+			name: "python",
+			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/python-logo.png",
+			id: 8
+		},
+		{
+			name: "rails",
+			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/rails-logo.png",
+			id: 9
+		},
+		{
+			name: "sass",
+			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/sass-logo.png",
+			id: 10
+		},
+		{
+			name: "sublime",
+			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/sublime-logo.png",
+			id: 11
+		},
+		{
+			name: "wordpress",
+			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/wordpress-logo.png",
+			id: 12
+		},
+	];
+    
+	Memory.init(cards);
 
-// Function to check if two flipped cards match
-function checkMatch() {
-    const [card1, card2] = flippedCards;
-    const image1 = card1.querySelector('span').textContent;
-    const image2 = card2.querySelector('span').textContent;
 
-    if (image1 === image2) {
-        matchedCards.push(card1, card2);
-        if (matchedCards.length === cards.length) {
-            alert('Congratulations! You have matched all the cards.');
-        }
-    } else {
-        // Hide the content again after a short delay
-        setTimeout(() => {
-            card1.classList.remove('flipped');
-            card2.classList.remove('flipped');
-            card1.querySelector('span').classList.add('hidden');
-            card2.querySelector('span').classList.add('hidden');
-        }, 500);
-    }
-
-    flippedCards = [];
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-    // Create game board
-    const gameBoard = document.getElementById('game-board');
-    cards.forEach((image, index) => {
-        const card = document.createElement('div');
-        card.classList.add('card');
-        card.dataset.index = index;
-
-        // Create content span for card
-        const content = document.createElement('span');
-        content.textContent = image;
-        content.classList.add('hidden'); // Initially hidden
-
-        // Append content to card
-        card.appendChild(content);
-
-        // Add click event listener to card
-        card.addEventListener('click', flipCard);
-
-        // Append card to game board
-        gameBoard.appendChild(card);
-    });
-
-    // Initialize the star rating display
-    updateStarRating();
-});
+})();
